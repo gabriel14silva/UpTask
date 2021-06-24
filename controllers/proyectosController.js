28eb5536ed7286c8cd5ea9dfde4c0e260c96ed03
Gabriel Griffin <jose.griffin@uraccan.edu.ni>
2021-06-24 11:24:58 -0600

    Version 1

---
+++ controllers/proyectosController.js
@@ -0,0 +1,15 @@
+exports.proyectosHome = (req, res) => {
+    res.render('index', {
+        nombrePagina: 'Proyectos'
+    });
+}
+
+exports.formularioProyecto = (req, res) => {
+    res.render('nuevoProyecto', {
+        nombrePagina: 'Nuevo Proyecto'
+    })
+}
+
+exports.nuevoProyecto = (req, res) => {
+ 
+}
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+//Importar el controlador
+const proyectosController = require
+    ('../controllers/proyectosController');
+
+module.exports = function () {
+    //ruta para el home
+    router.get('/', proyectosController.proyectosHome);
+    router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
+    router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
+
+    return router;
+}
+
